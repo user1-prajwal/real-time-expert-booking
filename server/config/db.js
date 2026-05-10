94ac7a3634203daea1c3de66375752262fc6a44f
user1-prajwal <ewitewit451@gmail.com>
2026-05-10 08:13:47 +0530

    fix: update MongoDB connection for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      tls: false,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
